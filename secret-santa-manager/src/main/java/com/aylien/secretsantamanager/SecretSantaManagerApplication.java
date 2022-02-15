4b92ab921228218d6b9705220cceadcbdf4ee56f
Darren Flannery <darrenflannery@hotmail.com>
2022-02-15 15:15:22 +0000

    Start Spring Boot project

---
+++ secret-santa-manager/src/main/java/com/aylien/secretsantamanager/SecretSantaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.aylien.secretsantamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSantaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretSantaManagerApplication.class, args);
+	}
+
+}
---
+++ secret-santa-manager/src/test/java/com/aylien/secretsantamanager/SecretSantaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aylien.secretsantamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
